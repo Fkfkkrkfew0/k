446b209af4d36fe4673066cdeb6b8f80369384ec
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-10-19 00:21:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("500272956677816320")
+let channel =     client.channels.get("500073966887763978")
 setInterval(function() {
 channel.send(`اني كفو`);
 }, 25)
